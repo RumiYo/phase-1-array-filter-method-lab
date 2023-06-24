13dbe87bdba701e042212fdacba38f1beba7e648
RumiYo <yokoyamarumi@gmail.com>
2023-06-23 23:17:26 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(driversNames, query){
+    query = query.toLowerCase();
+    const matchingDrivers =  driversNames.filter(function(driver){
+        return driver.toLowerCase().includes(query);
+    });
+    return matchingDrivers;
+}
+
+function fuzzyMatch(driversNames, query){
+    query = query.toLowerCase();
+    const matchingDrivers =  driversNames.filter(function(driver){
+        return driver.toLowerCase().startsWith(query);
+    });
+    return matchingDrivers;
+}
+function matchName(driversInfo, query){
+    const matchingDrivers = driversInfo.filter(function(driver){
+        return driver.name.includes(query);
+    })
+    return matchingDrivers;
+}
